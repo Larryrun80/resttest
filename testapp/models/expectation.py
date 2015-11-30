0cbd56c719b2079c91f3e986a1453780ecfad710
Larry Guo <larryrun80@gmail.com>
2015-11-30 15:43:52 +0800

    has key supported

--- testapp/models/expectation.py
+++ testapp/models/expectation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Filename: expectation.py
+import json
 
 from .error import RestTestError
 from .colortext import ColorText
@@ -30,23 +31,41 @@ class Expectation():
                 setattr(self, attr, None)
 
     def check_expectation(self, response):
-        if self.type == 'status_code':
-            return self.check_status_code(response)
-        pass
+        method_name = 'check_' + self.type
+        mtd = getattr(self, method_name)
+        return mtd(response)
 
     def check_status_code(self, response):
         if not self.value:
-            raise RestTestError('SOMETHING_MISSING', sth='ecpect value')
+            raise RestTestError('SOMETHING_MISSING', sth='expect value')
 
         if str(response.status_code) == str(self.value):
             self.print_result('SUCCESS')
         else:
             self.print_result('FAIL', 'get status code {0}'.format(
                             response.status_code))
-        pass
 
     def check_include_keys(self, response):
-        pass
+        print(self.value)
+        print(self.obj)
+        if not self.value or not self.obj:
+            raise RestTestError('SOMETHING_MISSING',
+                                sth='ecpect value or expect obj')
+
+        for key in self.value:
+            json_value = self.get_json(response)
+            if isinstance(json_value, dict) and key in json_value.keys():
+                self.print_result('SUCCESS', key=key)
+            elif isinstance(json_value, list):
+                has_key = True
+                for item in json_value:
+                    if key not in item.keys():
+                        has_key = False
+                        break
+                if has_key:
+                    self.print_result('SUCCESS', key=key)
+            else:
+                self.print_result('FAIL', key=key)
 
     def check_include_words(self, response):
         pass
@@ -54,12 +73,13 @@ class Expectation():
     def check_value(self, response):
         pass
 
-    def print_result(self, result, msg=None):
+    def print_result(self, result, msg=None, **params):
         ''' print_result
             this function is to print suitable result for expectation check
             pass 'SUCCESS' OR 'FAIL' as result
             pass fail message as msg
             msg only work in 'FAIL' type
+            params used to print statement info
         '''
         result_type = ('SUCCESS', 'FAIL')
         if result not in result_type:
@@ -73,6 +93,9 @@ class Expectation():
         if self.type == 'status_code':
             e_type = ColorText('status code', 'yellow')
             e_value = ColorText('be ' + str(self.value), 'yellow')
+        if self.type == 'include_keys':
+            e_type = ColorText(params['key'], 'yellow')
+            e_value = ColorText('in ' + str(self.obj), 'yellow')
         utils.print_log(m_statement.format(e_type, e_value))
 
         # print result
@@ -80,6 +103,27 @@ class Expectation():
         if result == 'SUCCESS':
             e_result = ColorText('successed', 'green')
         else:
-            m_result += ' {}'
+            if msg:
+                m_result += ' {}'
             e_result = ColorText('failed', 'red')
         utils.print_log(m_result.format(e_result, msg))
+
+    def get_json(self, response):
+        path = str(self.obj)
+        if not path.startswith('.'):
+            raise RestTestError('FORMAT_ERROR',
+                                correct_type='string start with "."')
+
+        json_value = response.json()
+        if path == '.':
+            return json_value
+
+        elements = path[1:].split('.')
+        try:
+            for element in elements:
+                json_value = json_value[element]
+        except:
+            raise RestTestError('KEY_NOT_FOUND',
+                                key=element,
+                                collection=json_value)
+        return json_value
